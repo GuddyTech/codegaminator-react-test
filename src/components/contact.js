c7e5c971aca400de6dae552e24906d577350d962
Okorie GoodGod <guddytechs@gmail.com>
2022-12-05 12:09:24 +0100

    added the contact

---
+++ src/components/contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from './card'
+
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+const Contact = () => {
+    return (
+        <div>
+            <h1> My Contacts </h1>
+            <Card names="Anthony" url="www.anthony.com" contact="+2345454345" img="https://www.google.com" />
+            <Card names="Allen" url="www.anthony.com" contact="+23433333333345" img="https://www.google.com" />
+            <Card names="Guddy" url="www.anthony.com" contact="+23499999954345" img="https://www.google.com" />
+            <h2>Another Section</h2>
+
+            <div>
+                <h3> {user.name} </h3>
+                <img src={user.imageUrl} alt='' width={user.imageSize} />
+            </div>
+        </div>
+
+    )
+}
+
+export default Contact
